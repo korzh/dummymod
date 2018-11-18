d87079cccaeadf0cb4b2ea70f97d59d8137d521d
Sergiy Korzh <sergey.korzh@live.com>
2018-11-18 09:37:55 +0200

    Update entry.ts

--- src/entry.ts
+++ src/entry.ts
@@ -1,17 +1,9 @@
 
 import * as utils1_int from "./utils1" 
-import * as utils2_int from './utils2'
 
 export let utils1 = utils1_int
-export let utils2 = utils2_int
 
 export * from './class1';
-export * from './class2';
 
-
-namespace dummymod {
-
-
-}
 
 
